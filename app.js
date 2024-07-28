3482fefea11bc8ae6a72df1ca808a5b9f38984a1
Manticora <999manticora999@gmail.com>
2024-07-28 20:06:20 +0300

    lesson_08 complete

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// клиент и сервер
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+    console.log(req.url, req.method);
+
+    // res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'application/json');
+
+    // res.write('<head><link rel="stylesheet" href="#"></head>');
+    // res.write('<h1>Welcome to local-server!</h1>');
+    // res.write('<p>My name is Sasha</p>');
+
+    const data = JSON.stringify([
+        { name: 'Tommy', age: 35 },
+        { name: 'Arthur', age: 40 },
+    ]);
+    // res.end();
+    res.end(data);
+});
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
