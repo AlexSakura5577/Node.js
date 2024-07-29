0c0e1b476187ebc56c6b645472a280fc37228b1f
Manticora <999manticora999@gmail.com>
2024-07-29 17:39:21 +0300

    lesson_12 complete

--- server.js
+++ server.js
@@ -3,36 +3,49 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-const routes = [
-    { path: '/', page: 'index' },
-    { path: '/home', page: 'index' },
-    { path: '/index.html', page: 'index' },
-    { path: '/contacts', page: 'contacts' },
-    { path: '/about-us', redirect: '/contacts' },
-];
+app.get('/', (req, res) => {
+    const title = 'Home';
+    res.render(createPath('index'), { title });
+});
 
-routes.forEach(({ path, page, redirect }) => {
-    if (redirect) {
-        app.get(path, (req, res) => {
-            res.redirect(redirect);
-        });
-    } else {
-        app.get(path, (req, res) => {
-            res.sendFile(createPath(page));
-        });
-    }
+app.get('/contacts', (req, res) => {
+    const title = 'Contacts';
+    const contacts = [
+        { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+        { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
+        { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
+    ];
+    res.render(createPath('contacts'), { contacts, title });
+});
+
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post';
+    res.render(createPath('post'), { title });
+});
+
+app.get('/posts', (req, res) => {
+    const title = 'Posts';
+    res.render(createPath('posts'), { title });
+});
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add Post';
+    res.render(createPath('add-post'), { title });
 });
 
 app.use((req, res) => {
+    const title = 'Error Page';
     res
         .status(404)
-        .sendFile(createPath('error'));
+        .render(createPath('error'), { title });
 });
