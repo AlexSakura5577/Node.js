1362c5682e50278470cffcacf0b388ec971911c7
Manticora <999manticora999@gmail.com>
2024-07-31 19:50:35 +0300

    lesson_20 complete

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 const express = require('express');
+const chalk = require('chalk');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const methodOverride = require('method-override');
 const postRoutes = require('./routes/post-routes');
 const postApiRoutes = require('./routes/api-post-routes');
 const contactRoutes = require('./routes/contact-routes');
 const createPath = require('./helpers/create-path');
 
+const errorMsg = chalk.bgKeyword('white').redBright;
+const successMsg = chalk.bgKeyword('green').white;
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
-const db = 'mongodb+srv://alex-sakura:n0RPA95D6s0algf9@cluster0.ah5pkdj.mongodb.net/node-blog?retryWrites=true&w=majority';
-
 mongoose
-  .connect(db)
-  .then((res) => console.log('Connected to DB'))
-  .catch((error) => console.log(error));
+  .connect(process.env.MONGO_URL)
+  .then((res) => console.log(successMsg('Connected to DB')))
+  .catch((error) => console.log(errorMsg(error)));
 
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
+app.listen(process.env.PORT, (error) => {
+  error ? console.log(errorMsg(error)) : console.log(successMsg(`listening port ${process.env.PORT}`));
 });
 
 app.use(express.urlencoded({ extended: false }));
